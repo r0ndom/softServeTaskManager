c5c4ab2d8135ad960ea93c1e0d24427244ded3d1
r0ndom <r0ndom@yahoo.com>
2015-07-20 21:34:38 +0300

    added authentication test

---
+++ src/test/java/com/softserve/taskmanager/service/UserAuthenticationProviderTest.java
@@ -0,0 +1,79 @@
+package com.softserve.taskmanager.service;
+
+import com.softServe.taskManager.model.User;
+import com.softServe.taskManager.service.UserService;
+import com.softServe.taskManager.service.security.UserAuthenticationProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+public class UserAuthenticationProviderTest {
+
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    UserAuthenticationProvider userAuthenticationProvider;
+    @Mock
+    private UserService userService;
+    @Mock
+    private Authentication authentication;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticateEmailEmpty() {
+        userAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticatePasswordEmpty() {
+        when(authentication.getName()).thenReturn(EMAIL);
+        userAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticateNoUser() {
+        when(authentication.getName()).thenReturn(EMAIL);
+        when(authentication.getCredentials()).thenReturn(PASSWORD);
+        userAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAuthenticateWrongPassword() {
+        User user = new User("mail", "1234", null);
+        when(authentication.getName()).thenReturn(EMAIL);
+        when(authentication.getCredentials()).thenReturn(PASSWORD);
+        when(userService.findByEmail(EMAIL)).thenReturn(user);
+        userAuthenticationProvider.authenticate(authentication);
+    }
+
+    @Test
+    public void testAuthenticate() {
+        User user = new User(EMAIL, String.valueOf(PASSWORD.hashCode()), null);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(user, PASSWORD.hashCode(), null);
+        when(authentication.getName()).thenReturn(EMAIL);
+        when(authentication.getCredentials()).thenReturn(PASSWORD);
+        when(userService.findByEmail(EMAIL)).thenReturn(user);
+        assertEquals(authenticationToken, userAuthenticationProvider.authenticate(authentication));
+    }
+
+}
